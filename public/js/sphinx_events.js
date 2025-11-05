6fe06f3b03b8bb436051a1bd5b93f7d0a2ae1303
aryanm9026 <aryanmishra010@gmail.com>
2025-11-06 00:45:25 +0530

    Some new events added

--- public/js/sphinx_events.js
+++ public/js/sphinx_events.js
@@ -168,5 +168,133 @@ const extractedEvents = [
     eventName: "SparkX",
     venue: "VLTC 2ND FLOOR",
     dates: ["8NOV"]
+  },
+// New events that added afterwards
+  {
+    eventName: "Decode The Scam",
+    venue: "VLTC L104",
+    dates: ["7NOV"]
+  },
+  {
+    eventName: "Electro Hunt",
+    venue: "VLTC L007",
+    dates: ["7NOV"]
+  },
+  {
+    eventName: "Corporate Catalyst",
+    venue: "VLTC L103",
+    dates: ["7NOV"]
+  },
+  {
+    eventName: "Bridge Busters",
+    venue: "VLTC L002",
+    dates: ["7NOV"]
+  },
+  {
+    eventName: "Interactive Installation Challenge",
+    venue: "VLTC L009",
+    dates: ["7NOV"]
+  },
+  {
+    eventName: "Matquiz - The Materials Science Quiz",
+    venue: "VLTC L008",
+    dates: ["8NOV"]
+  },
+  {
+    eventName: "Tambola",
+    venue: "VLTC L104",
+    dates: ["8NOV"]
+  },
+  {
+    eventName: "Ad Mania",
+    venue: "VLTC L103",
+    dates: ["8NOV"]
+  },
+  {
+    eventName: "Tremor Tech",
+    venue: "VLTC L008",
+    dates: ["9NOV"]
+  },
+  {
+    eventName: "The Chemical Challenge",
+    venue: "VLTC L103",
+    dates: ["9NOV"]
+  },
+  {
+    eventName: "Type Racer",
+    venue: "VLTC L004",
+    dates: ["9NOV"]
+  },
+  {
+    eventName: "Phaseshift",
+    venue: "VLTC L006",
+    dates: ["9NOV"]
+  },
+  {
+    eventName: "Cad Master",
+    venue: "VLTC L104",
+    dates: ["9NOV"]
+  },
+  {
+    eventName: "Bidding War",
+    venue: "VLTC L103",
+    dates: ["9NOV"]
+  },
+  // more events
+ {
+    eventName: "Ai Experience",
+    venue: "VLTC L006",
+    dates: ["8NOV"]
+  },
+  {
+    eventName: "Power Of Standards",
+    venue: "VLTC L008",
+    dates: ["8NOV"]
+  },
+  {
+    eventName: "Solvent Stories",
+    venue: "VLTC L004",
+    dates: ["7NOV"]
+  },
+  {
+    eventName: "Laser Tag",
+    venue: "CHESS ROOM",
+    dates: ["7NOV", "8NOV", "9NOV"]
+  },
+  {
+    eventName: "Silent Disco",
+    venue: "WORLD E-SPORTS HALL",
+    dates: ["7NOV", "8NOV", "9NOV"]
+  },
+  {
+    eventName: "Room Of Nostalgia",
+    venue: "VIKRAM SARABHAI",
+    dates: ["7NOV", "8NOV", "9NOV"]
+  },
+  {
+    eventName: "Fun Fusion Arena",
+    venue: "VLTC 2.0 FLOOR",
+    dates: ["7NOV", "8NOV", "9NOV"]
+  },
+  {
+    eventName: "Turbo Mania",
+    venue: "SAC LAWN",
+    dates: ["7NOV", "8NOV", "9NOV"]
+  },
+  {
+    eventName: "Magic Of Ice",
+    venue: "VLTC L002",
+    dates: ["8NOV"]
+  },
+  {
+    eventName: "Physics Escape Room",
+    venue: "VLTC L106",
+    dates: ["8NOV"]
+  },
+  {
+    eventName: "Cryx Blackout Protocol",
+    venue: "VLTC L106",
+    dates: ["9NOV"]
   }
 ];
+
